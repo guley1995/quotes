d43694642c196d3f369a0eee265405a033d6783e
guley1995 <91196638+guley1995@users.noreply.github.com>
2023-03-23 16:15:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 async function loadQuote() {
-    const response = await fetch('https://quotes.rest/qod?category=inspire&' + Math.random());
-    const data = await response.json();
-    const quote = data.contents.quotes[0].quote;
-    document.getElementById('quote').innerHTML = quote;
+  const response = await fetch('https://quotes.rest/qod?category=inspire', {
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  });
+  const data = await response.json();
+  const quote = data.contents.quotes[0].quote;
+  document.getElementById('quote').innerHTML = quote;
 }
